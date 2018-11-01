06f91382816a627565611840a90b9f97a548d36b
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-11-01 12:29:22 +0100

    Complete tests for reset functionnalities

--- test/unit/lib/exec.test.js
+++ test/unit/lib/exec.test.js
@@ -7,23 +7,23 @@ var sliceIndex = 0;
 var shared = {
   scanTimeout: 15,
   bluetoothOn: true,
-  scanTimer : null
+  scanTimer: null
 };
 
-var connectMock = function(peripheral) {
+var connectMock = function (peripheral) {
   return Promise.resolve(peripheral);
 };
-var discoverServicesMock = function(peripheral) {
-  return Promise.resolve([ peripheral, peripheral ]);
+var discoverServicesMock = function (peripheral) {
+  return Promise.resolve([peripheral, peripheral]);
 };
-var discoverCharacteristicsMock = function(peripheral, service) {
-  return Promise.resolve([ peripheral, service ]);
+var discoverCharacteristicsMock = function (peripheral, service) {
+  return Promise.resolve([peripheral, service]);
 };
-var sendMock = function(peripheral, characteristic, command) {
+var sendMock = function (peripheral, characteristic, command) {
   assert.deepEqual(command.slice(0, sliceIndex), expectedCommand.slice(0, sliceIndex), 'Not expected command sent');
   return Promise.resolve(peripheral);
 };
-var scanMock = function(peripheral) {
+var scanMock = function (peripheral) {
   if (foundPeripherals !== undefined) {
     return Promise.resolve(foundPeripherals);
   } else {
@@ -31,31 +31,31 @@ var scanMock = function(peripheral) {
   }
 };
 
-var exec = proxyquire('../../../lib/exec.js', { 
+var exec = proxyquire('../../../lib/exec.js', {
   './shared.js': shared,
-  './bluetooth.connect.js' : connectMock,
-  './bluetooth.discoverServices.js' : discoverServicesMock,
-  './bluetooth.discoverCharacteristics.js' : discoverCharacteristicsMock,
-  './bluetooth.send.js' : sendMock,
-  './bluetooth.scan.js' : scanMock
+  './bluetooth.connect.js': connectMock,
+  './bluetooth.discoverServices.js': discoverServicesMock,
+  './bluetooth.discoverCharacteristics.js': discoverCharacteristicsMock,
+  './bluetooth.send.js': sendMock,
+  './bluetooth.scan.js': scanMock
 });
 
-describe('Gladys device exec', function() {
+describe('Gladys device exec', function () {
 
-  beforeEach(function() {
+  beforeEach(function () {
     expectedCommand = null;
     sliceIndex = 0;
     foundPeripherals = undefined;
   });
 
-  it('Device identifier not managed', function(done) {
+  it('Device identifier not managed', function (done) {
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'unknown'
       },
-      state : {
-        value : 3
+      state: {
+        value: 3
       }
     };
 
@@ -67,14 +67,14 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is binary with not managed value', function(done) {
+  it('Device identifier is binary with not managed value', function (done) {
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'binary'
       },
-      state : {
-        value : 3
+      state: {
+        value: 3
       }
     };
 
@@ -86,17 +86,17 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is switch with value = 0', function(done) {
+  it('Device identifier is switch with value = 0', function (done) {
     expectedCommand = shared.commands.off.slice(0);
     sliceIndex = expectedCommand.length - 1;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'switch'
       },
-      state : {
-        value : 0
+      state: {
+        value: 0
       }
     };
 
@@ -108,17 +108,17 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is switch with value = 1', function(done) {
+  it('Device identifier is switch with value = 1', function (done) {
     expectedCommand = shared.commands.on.slice(0);
     sliceIndex = expectedCommand.length - 1;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'switch'
       },
-      state : {
-        value : 1
+      state: {
+        value: 1
       }
     };
 
@@ -130,7 +130,7 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 0 (black / min)', function(done) {
+  it('Device identifier is color with value = 0 (black / min)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[10] = 0x00;
     expectedCommand[11] = 0x00;
@@ -138,12 +138,12 @@ describe('Gladys device exec', function() {
     sliceIndex = 14;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 0
+      state: {
+        value: 0
       }
     };
 
@@ -155,19 +155,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 16.777.215 (white / max)', function(done) {
+  it('Device identifier is color with value = 16.777.215 (white / max)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0xFF;
     expectedCommand[10] = 0xFF;
     expectedCommand[11] = 0xFF;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 16777215
+      state: {
+        value: 16777215
       }
     };
 
@@ -179,7 +179,7 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 16711680 (red)', function(done) {
+  it('Device identifier is color with value = 16711680 (red)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0xFF;
     expectedCommand[10] = 0x00;
@@ -187,12 +187,12 @@ describe('Gladys device exec', function() {
     sliceIndex = 14;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 16711680
+      state: {
+        value: 16711680
       }
     };
 
@@ -204,19 +204,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 65280 (green)', function(done) {
+  it('Device identifier is color with value = 65280 (green)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0x00;
     expectedCommand[10] = 0xFF;
     expectedCommand[11] = 0x00;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 65280
+      state: {
+        value: 65280
       }
     };
 
@@ -228,7 +228,7 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 255 (blue)', function(done) {
+  it('Device identifier is color with value = 255 (blue)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0x00;
     expectedCommand[10] = 0x00;
@@ -236,12 +236,12 @@ describe('Gladys device exec', function() {
     sliceIndex = 14;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 255
+      state: {
+        value: 255
       }
     };
 
@@ -253,19 +253,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 16776960 (yellow)', function(done) {
+  it('Device identifier is color with value = 16776960 (yellow)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0xFF;
     expectedCommand[10] = 0xFF;
     expectedCommand[11] = 0x00;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 16776960
+      state: {
+        value: 16776960
       }
     };
 
@@ -277,7 +277,7 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 65535 (cyan)', function(done) {
+  it('Device identifier is color with value = 65535 (cyan)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0x00;
     expectedCommand[10] = 0xFF;
@@ -285,12 +285,12 @@ describe('Gladys device exec', function() {
     sliceIndex = 14;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 65535
+      state: {
+        value: 65535
       }
     };
 
@@ -302,7 +302,7 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 16711935 (magenta)', function(done) {
+  it('Device identifier is color with value = 16711935 (magenta)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0xFF;
     expectedCommand[10] = 0x00;
@@ -310,12 +310,12 @@ describe('Gladys device exec', function() {
     sliceIndex = 14;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 16711935
+      state: {
+        value: 16711935
       }
     };
 
@@ -327,7 +327,7 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is color with value = 8355711 (grey)', function(done) {
+  it('Device identifier is color with value = 8355711 (grey)', function (done) {
     expectedCommand = shared.commands.color.slice(0);
     expectedCommand[09] = 0x7F;
     expectedCommand[10] = 0x7F;
@@ -335,12 +335,12 @@ describe('Gladys device exec', function() {
     sliceIndex = 14;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'color'
       },
-      state : {
-        value : 8355711
+      state: {
+        value: 8355711
       }
     };
 
@@ -352,16 +352,16 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is manual', function(done) {
+  it('Device identifier is manual', function (done) {
     expectedCommand = 'MANUAL COMMAND';
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'manual'
       },
-      state : {
-        value : 'MANUAL COMMAND'
+      state: {
+        value: 'MANUAL COMMAND'
       }
     };
 
@@ -373,19 +373,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is brightness with level = 0% (min / 600)', function(done) {
+  it('Device identifier is brightness with level = 0% (min / 600)', function (done) {
     expectedCommand = shared.commands.brightness.slice(0);
     expectedCommand[8] = 0x02;
     expectedCommand[9] = 0x58;
     sliceIndex = 10;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'brightness'
       },
-      state : {
-        value : 0
+      state: {
+        value: 0
       }
     };
 
@@ -397,19 +397,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is brightness with level = 25% (1213)', function(done) {
+  it('Device identifier is brightness with level = 25% (1213)', function (done) {
     expectedCommand = shared.commands.brightness.slice(0);
     expectedCommand[8] = 0x04;
     expectedCommand[9] = 0xBD;
     sliceIndex = 10;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'brightness'
       },
-      state : {
-        value : 25
+      state: {
+        value: 25
       }
     };
 
@@ -421,19 +421,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is brightness with level = 50% (1825)', function(done) {
+  it('Device identifier is brightness with level = 50% (1825)', function (done) {
     expectedCommand = shared.commands.brightness.slice(0);
     expectedCommand[8] = 0x07;
     expectedCommand[9] = 0x21;
     sliceIndex = 10;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'brightness'
       },
-      state : {
-        value : 50
+      state: {
+        value: 50
       }
     };
 
@@ -445,19 +445,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is brightness with level = 75% (2438)', function(done) {
+  it('Device identifier is brightness with level = 75% (2438)', function (done) {
     expectedCommand = shared.commands.brightness.slice(0);
     expectedCommand[8] = 0x09;
     expectedCommand[9] = 0x86;
     sliceIndex = 10;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'brightness'
       },
-      state : {
-        value : 75
+      state: {
+        value: 75
       }
     };
 
@@ -469,19 +469,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Device identifier is brightness with level = 100% (max / 3050)', function(done) {
+  it('Device identifier is brightness with level = 100% (max / 3050)', function (done) {
     expectedCommand = shared.commands.brightness.slice(0);
     expectedCommand[8] = 0x0B;
     expectedCommand[9] = 0xEA;
     sliceIndex = 10;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'brightness'
       },
-      state : {
-        value : 100
+      state: {
+        value: 100
       }
     };
 
@@ -493,18 +493,58 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('No peripherals found', function(done) {
+  it('Device identifier is color reset', function (done) {
+    expectedCommand = shared.commands.colorReset.slice(0);
+    sliceIndex = 14;
+
+    var deviceInfo = {
+      deviceType: {
+        identifier: 'Peripheral 1',
+        deviceTypeIdentifier: 'color_reset'
+      },
+      state: {}
+    };
+
+    exec(deviceInfo)
+      .then(() => {
+        done();
+      }).catch((result) => {
+        done('Should not have fail : ' + result);
+      });
+  });
+
+  it('Device identifier is white reset', function (done) {
+    expectedCommand = shared.commands.whiteReset.slice(0);
+    sliceIndex = 14;
+
+    var deviceInfo = {
+      deviceType: {
+        identifier: 'Peripheral 1',
+        deviceTypeIdentifier: 'white_reset'
+      },
+      state: {}
+    };
+
+    exec(deviceInfo)
+      .then(() => {
+        done();
+      }).catch((result) => {
+        done('Should not have fail : ' + result);
+      });
+  });
+
+  it('No peripherals found', function (done) {
     expectedCommand = shared.commands.off.slice(0);
     sliceIndex = expectedCommand.length - 1;
     foundPeripherals = false;
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'switch'
       },
-      state : {
-        value : 0
+      state: {
+        value: 0
       }
     };
 
@@ -516,19 +556,19 @@ describe('Gladys device exec', function() {
       });
   });
 
-  it('Expected peripheral not found', function(done) {
+  it('Expected peripheral not found', function (done) {
     expectedCommand = shared.commands.off.slice(0);
     sliceIndex = expectedCommand.length - 1;
     foundPeripherals = new Map();
     foundPeripherals.set('unknow', null);
 
     var deviceInfo = {
-      deviceType : {
+      deviceType: {
         identifier: 'Peripheral 1',
         deviceTypeIdentifier: 'switch'
       },
-      state : {
-        value : 0
+      state: {
+        value: 0
       }
     };
 
@@ -539,4 +579,5 @@ describe('Gladys device exec', function() {
         done();
       });
   });
+
 });
