ab42bb8c29ca6690e609c80c711c952069a3d78a
Alexandre Trovato <alexandre.trovato@gmail.com>
2019-02-03 09:57:16 +0100

    Code cleaning

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const noble = require('noble');
 const shared = require('./lib/shared.js');
+const exec = require('./lib/exec.js');
+const install = require('./lib/install.js');
 
 module.exports = function (sails) {
-  var exec = require('./lib/exec.js');
-  var install = require('./lib/install.js');
-
   gladys.on('ready', function () {
     noble.on('stateChange', function (state) {
       if (state === 'poweredOn') {
         shared.bluetoothOn = true;
-        sails.log.info('Bluetooth device available');
+        sails.log.info('AwoX module: Bluetooth device available');
       } else if (state === 'poweredOff') {
         shared.bluetoothOn = false;
-        sails.log.warn('Bluetooth device not available');
+        sails.log.warn('AwoX module: Bluetooth device not available');
         noble.stopScanning();
       }
     });
