5fc70b4f078f79da19796898a33cf6f55872eb15
Alexandre Trovato <alexandre.trovato@gmail.com>
2019-02-20 10:20:27 +0100

    Fix lang errors

--- assets/js/awox.lang.js
+++ assets/js/awox.lang.js
@@ -11,22 +11,26 @@ var translationsEN = {
   brightness: 'Brightness',
   color: 'Color',
   color_reset: 'Color reset',
+  preset: 'Preset color sequences',
+  reset: 'Device reset (unpair)',
   MESH_PAIR_DESCR: 'To be able to pair an AwoX Mesh device, be sure that the current device is unpaired and wait for it (red ligth).'
 };
 
 var translationsFR = {
   SERVICE_FAIL: 'Le service AwoX n\'est pas disponible, vérifiez son installation',
-  SCAN_TITLE: 'Analyse des péripéhriques compatibles',
+  SCAN_TITLE: 'Analyse des périphériques compatibles',
   SCAN_START: 'Démarrer l\'analyse',
   SCAN_RUNNING: 'Analyse en cours...',
   SCAN_UNAVAILABLE: 'Le Bluetooth n\'est pas actif',
-  SCAN_NO_FOUND: 'Aucun péripherique n\'a été trouvé',
+  SCAN_NO_FOUND: 'Aucun périphérique n\'a été trouvé',
   DEVICE_CONFIG: 'Configuration du périphérique',
   switch: 'Interrupteur',
   white_reset: 'Remise à zéro du blanc',
   brightness: 'Luminosité',
   color: 'Couleur',
   color_reset: 'Remise à zéro de la couleur',
+  preset: 'Sequences de couleur définies',
+  reset: 'Remise à zéro du périphérique (unpair)',
   MESH_PAIR_DESCR: 'Afin de lier correctement un périphérique AwoX Mesh, soyez sûr que celui-ci n\'est pas déjà appairé et qu\'il soit en attente de le dvenir (lumière rouge).'
 };
 
